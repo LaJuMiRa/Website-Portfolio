34585bd2a61ea2ae3d1c750e33154e10da8184bf
Laurenz Rauscher <laurenz.rauscher@gmail.com>
2026-03-26 20:32:33 +0100

    Navbar changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ import ThemeToggle from './ThemeToggle'
 
 const navLinks = [
   { href: '#about',      label: 'About' },
-  { href: '#skills',     label: 'Skills' },
+  //{ href: '#skills',     label: 'Skills' },
   { href: '#experience', label: 'Experience' },
   { href: '#projects',   label: 'Projects' },
   { href: '#contact',    label: 'Contact' },
