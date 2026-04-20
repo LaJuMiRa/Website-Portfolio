3e40d8ed3597a37503a7ea25d8188eee3628c1ca
Laurenz Rauscher <laurenz.rauscher@gmail.com>
2026-04-20 14:10:45 +0200

    repo update

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Experience from '@/components/Experience'
 import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 import BackToTop from '@/components/BackToTop'
-
 export default function Home() {
   return (
     <>
